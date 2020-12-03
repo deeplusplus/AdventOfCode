6aac334478d065a26e8126e1576c7923d714b4b9
deeplusplus <r2dan2d2@gmail.com>
2020-12-03 18:24:13 -0500

    Advent of code day 3 part 2

--- Day_3/Day_3.py
+++ Day_3/Day_3.py
@@ -17,8 +17,8 @@ def create_grid_from_file(file_name):
         y += 1
     return { 'grid': map_points, 'map_height': len(lines), 'map_width': len(lines[0]) - 1  }
 
-def next_toboggan_spot(point):
-    return (point[0] + 3, point[1] + 1)
+def next_toboggan_spot(point, trajectory):
+    return (point[0] + trajectory[0], point[1] + trajectory[1])
 
 def spot_has_tree(point, grid):
     return not grid[point]
@@ -27,10 +27,11 @@ def main():
     tree_count = 0
     grid = create_grid_from_file('input1.txt')
     point = (0, 0)
+    trajectory = (7, 1)
 
     while point[1] < grid['map_height']:
         if spot_has_tree(point, grid['grid']): tree_count += 1
-        point = next_toboggan_spot(point)
+        point = next_toboggan_spot(point, trajectory)
 
     print(tree_count)
 
