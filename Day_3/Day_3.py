9407de3b1983823a13004b22be039628b5ebf8e9
deeplusplus <r2dan2d2@gmail.com>
2020-12-03 18:16:23 -0500

    Advent day 3 part 1

---
+++ Day_3/Day_3.py
@@ -0,0 +1,39 @@
+# (x, y, is_empty_space)
+
+
+
+def create_grid_from_file(file_name):
+    file = open(file_name, 'r')
+    lines = file.readlines()
+    x = 0
+    y = 0
+    map_points = {}
+
+    for line in lines:
+        x = 0
+        for char in line:
+            map_points[(x, y)] = char is '.'
+            x += 1
+        y += 1
+    return { 'grid': map_points, 'map_height': len(lines), 'map_width': len(lines[0]) - 1  }
+
+def next_toboggan_spot(point):
+    return (point[0] + 3, point[1] + 1)
+
+def spot_has_tree(point, grid):
+    return not grid[point]
+
+def main():
+    tree_count = 0
+    grid = create_grid_from_file('input1.txt')
+    point = (0, 0)
+
+    while point[1] < grid['map_height']:
+        if spot_has_tree(point, grid['grid']): tree_count += 1
+        point = next_toboggan_spot(point)
+
+    print(tree_count)
+
+
+if __name__ == "__main__":
+    main()
