d58e1df2d77984f554b0869dca94af05662b90f2
deeplusplus <r2dan2d2@gmail.com>
2020-12-06 13:58:54 -0500

    Day 6 part 2

--- Day_6/questionaireParser.js
+++ Day_6/questionaireParser.js
@@ -1,35 +1,45 @@
 module.exports = {
     parseQuestionaires: function (inputString) {
         var inputSplitOnNewLine = inputString.split('\n');
-        var groupedAnswers = this.coalesceToGroups(inputSplitOnNewLine);
+        var groups = this.coalesceToGroups(inputSplitOnNewLine);
         var count = 0;
 
-        for(let answer of groupedAnswers) {
-            count = count + this.calculateGroupCount(answer);
+        for(let group of groups) {
+            count = count + this.calculateGroupCount(group);
         }
 
+        console.log(groups);
         return count;
     },
     coalesceToGroups: function(listOfInputLines) {
         let collectedAnswers = []
-        let groupsAnswers = '';
+        let groupAnswer = '';
+        let countOfPeopleInGroup = 0;
         for(let line of listOfInputLines) {
             if(line === '') {
-                collectedAnswers.push(groupsAnswers);
-                groupsAnswers = '';
+                collectedAnswers.push({
+                    count: countOfPeopleInGroup,
+                    answers: groupAnswer});
+                groupAnswer = '';
+                countOfPeopleInGroup = 0;
             }
             else {
-                groupsAnswers = groupsAnswers + line;
+                countOfPeopleInGroup = countOfPeopleInGroup + 1;
+                groupAnswer = groupAnswer + line;
             }
         }
         return collectedAnswers;
     },
-    calculateGroupCount: function(groupAnswer) {
-        let groupSet = new Set();
-        for(let char of groupAnswer) {
-            groupSet.add(char);
+    calculateGroupCount: function(group) {
+        let alphabet = 'qwertyuiopasdfghjklzxcvbnm';
+        let count = 0;
+        for(let char of alphabet) {
+            let countOfCharInGroup = (group.answers.match(new RegExp(char, "g")) || []).length;
+            if(countOfCharInGroup == group.count){
+                count = count + 1;
+            }
         }
-        return groupSet.size;
+        return count;
     }
 
 }
