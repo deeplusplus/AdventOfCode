3388650648a12aabde05119f292e7c1f794bb29f
deeplusplus <r2dan2d2@gmail.com>
2020-12-07 22:48:09 -0500

    My recursion was stopping at the first encountered node instead of traversing the whole tree.

--- Day_7/day_7.py
+++ Day_7/day_7.py
@@ -1,44 +1,48 @@
 import re
 
-# { container_bag: color, contents: [(name, count), (name, count)]}
-
-def bag_contents_include_shiny_gold(contents):
-    for rule_tuple in contents:
-        if rule_tuple[0] == 'shiny gold':
+def bag_contains_shiny_gold(bag_name, bag_contents, rule_dict):
+    # print('Checking:', bag_name, '\nContents:', bag_contents, '\n')
+    if 'shiny gold' in bag_contents:
+        return True
+    
+    for inner_bag_name in bag_contents:
+        if bag_contains_shiny_gold(inner_bag_name, rule_dict[inner_bag_name], rule_dict):
             return True
+        else:
+            pass
+    return False
 
-def rule_has_shiny_gold(rule, rules):
-    if rule['container_bag'] == 'shiny gold':
-        return True
-    elif bag_contents_include_shiny_gold(rule['contents']):
-        return True
-    elif len(rule['contents']) is 0:
-        return False
 
 
 def process_line_to_rule(line):
     split_line = line.split('bag')
-    rule = { 'container_bag': split_line[0].strip(), 'contents': [] }
+    rule_name = split_line[0].strip()
+    contents = []
 
     if split_line[1] is not 's contain no other':
         for chunk in split_line:
             match = re.search('[0-9]', chunk)
             if match:
-                rule['contents'].append((chunk[match.start() + 2:-1], chunk[match.start()]))
+                # contents.append((chunk[match.start() + 2:-1], chunk[match.start()]))
+                contents.append(chunk[match.start() + 2:-1])
 
-    return rule
+    return (rule_name, contents)
 
 def main():
-    file = open('testinput.txt')
+    file = open('input.txt')
     lines = file.readlines()
-    rules = []
+    rules = {}
     count = 0
 
     for line in lines:
-        rules.append(process_line_to_rule(line))
+        rule_tuple = process_line_to_rule(line)
+        print(rule_tuple)
+        rules[rule_tuple[0]] = rule_tuple[1]
 
-    for rule in rules:
-        if rule_has_shiny_gold(rule, rules):
+
+    for key, value in rules.items():
+        # print('-----------------------')
+        if bag_contains_shiny_gold(key, value, rules):
             count += 1
 
     print(count)
